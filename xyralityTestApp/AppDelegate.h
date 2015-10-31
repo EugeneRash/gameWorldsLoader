d0ab860f87e3727f3b80065ca42b171c67c8bd51
Eugene Golub <golub.e@epsysoft.com.ua>
2015-10-31 16:16:54 +0200

    Initial Commit

---
+++ xyralityTestApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xyralityTestApp
+//
+//  Created by rash on 10/31/15.
+//  Copyright © 2015 rash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xyralityTestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xyralityTestApp
+//
+//  Created by rash on 10/31/15.
+//  Copyright © 2015 rash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
